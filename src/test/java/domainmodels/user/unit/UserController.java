2a1a91a634e5c02b68c28a554fd43df92e0b1afc
Yves  Kaufmann <yk@Yvess-MacBook-Pro.local>
2020-04-11 10:41:49 +0200

    added Unittests for User

---
+++ src/test/java/domainmodels/user/unit/UserController.java
@@ -0,0 +1,42 @@
+package domainmodels.user.unit;
+
+
+import ch.course223.advanced.domainmodels.user.UserService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserController {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Before
+    public void setUp(){
+
+    }
+
+    @Test
+    @WithMockUser
+    public void clearlySucessful() {
+
+    }
+
+}
