7da5f1cf63e7a8398a3db50cc9b994a8a1c2f94a
Aschvin <aschvin.umachandran@edu.tbz.ch>
2020-04-11 11:47:06 +0200

    added flyway

--- build/generated/sources/annotationProcessor/java/main/ch/course223/advanced/domainmodels/user/mapper/UserMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/ch/course223/advanced/domainmodels/user/mapper/UserMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-04-11T11:12:10+0200",
+    date = "2020-04-11T11:28:43+0200",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 13.0.1 (Private Build)"
 )
 @Component
---
+++ src/main/resources/db/migration/V2__DML.sql
@@ -0,0 +1,14 @@
+INSERT INTO public.authority VALUES ('4da5473b-09b4-4941-9556-e75f439ccc39', 'USER_CREATE');
+INSERT INTO public.authority VALUES ('b85ab915-33ed-445f-8d0c-ec8a07505a5a', 'USER_MODIFY_GLOBAL');
+INSERT INTO public.authority VALUES ('d8ced773-78b5-41c8-9c0a-0482de29b51f', 'USER_MODIFY_OWN');
+INSERT INTO public.authority VALUES ('c40a573a-a6bd-413f-b573-64338770e261', 'USER_DEACTIVATE');
+INSERT INTO public.authority VALUES ('1f7021af-c92b-4c88-843e-862fb5cc9204', 'USER_SEE_GLOBAL');
+INSERT INTO public.authority VALUES ('dfd2440e-eddf-488a-b4a7-225b6e21a484', 'USER_SEE_OWN');
+
+
+INSERT INTO public.role VALUES ('7ea6108f-6163-46ae-8b1f-1adbb1daaef6', 'ADMIN');
+INSERT INTO public.role VALUES ('a807f074-53bc-4aa7-a6d2-341928b7dd2a', 'USER');
+
+
+INSERT INTO public.users VALUES ('2643dbc0-cd29-4446-84cb-0c98aed89fb8', '2020-12-12', '2020-12-12', 'john.doe@noseryoung.ch', 1, 'John', 'Doe', 0, '$2a$10$XKIRmT.3a4FqYIsFE88opOtNBbzmTxEKSWhsBoT1iUZhtgYqNCS.O');
+INSERT INTO public.users VALUES ('183bd877-f871-4000-b9c7-a7ce92991b5f', '2020-12-12', '2020-12-12', 'jane.doe@noseryoung.ch', 1, 'Jane', 'Doe', 0, '$2a$10$XKIRmT.3a4FqYIsFE88opOtNBbzmTxEKSWhsBoT1iUZhtgYqNCS.O');
