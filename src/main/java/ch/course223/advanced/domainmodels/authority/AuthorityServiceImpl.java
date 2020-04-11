6fce5e078951080875d6562aa1fde7aa55ab886b
Aschvin <aschvin.umachandran@edu.tbz.ch>
2020-04-11 10:42:57 +0200

    removed unnecessary imports

--- src/main/java/ch/course223/advanced/domainmodels/authority/AuthorityServiceImpl.java
+++ src/main/java/ch/course223/advanced/domainmodels/authority/AuthorityServiceImpl.java
@@ -1,7 +1,6 @@
 package ch.course223.advanced.domainmodels.authority;
 
 import ch.course223.advanced.core.ExtendedServiceImpl;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
