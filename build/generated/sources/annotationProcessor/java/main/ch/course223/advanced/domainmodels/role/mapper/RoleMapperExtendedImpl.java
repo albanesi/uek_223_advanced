7c5671bd376221aca4bce3f02ad356ceb6d97255
Yves  Kaufmann <yk@Yvess-MacBook-Pro.local>
2020-04-12 08:48:19 +0200

    added all tests

--- build/generated/sources/annotationProcessor/java/main/ch/course223/advanced/domainmodels/role/mapper/RoleMapperExtendedImpl.java
+++ build/generated/sources/annotationProcessor/java/main/ch/course223/advanced/domainmodels/role/mapper/RoleMapperExtendedImpl.java
@@ -13,8 +13,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-04-12T00:22:39+0200",
-    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 13.0.1 (Private Build)"
+    date = "2020-04-12T08:47:48+0200",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 13.0.2 (Oracle Corporation)"
 )
 @Component
 public class RoleMapperExtendedImpl implements RoleMapperExtended {
